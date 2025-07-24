75d74e56519d7e297f9e03cccfec5e9098ec3a12
Ahmed-HA-RE <ah607k@gmail.com>
2025-07-24 17:52:46 +0400

    chore(env):chore: add .env file, update .gitignore and import the variable in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import CoinCards from './components/CoindCard';
-const API_URL =
-  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc &per_page=10&page=1&sparkline=false';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
   const [coins, setCoins] = useState([]);
@@ -12,7 +11,9 @@ const App = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const { data } = await axios.get(API_URL);
+        const { data } = await axios.get(
+          `${API_URL}&order=market_cap_desc &per_page=10&page=1&sparkline=false`
+        );
         setCoins(data);
       } catch (error) {
         const status = error.response ? error.response.status : null;
